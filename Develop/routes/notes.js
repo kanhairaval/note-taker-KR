a8923bc2ce96360c36a8ebfd5ffafebc3c61b98e
kanhairaval <kanhairaval@gmail.com>
2023-01-25 04:38:04 -0500

    unexpected bug in fs file

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,18 +1,54 @@
 const notes = require("express").Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend } = require('../helper/fsUtils');
+const uuid = require('../helper/uuid');
 
 notes.post("/", (req, res) => {
-  res.json(req.body);
+
   if (req.body) {
     const newNote = {
-      title,
-      text,
+      title: req.body.title,
+      text: req.body.text,
+      note_id: uuid(),
   }
 
   readAndAppend(newNote, './db/db.json');
-    res.json(`Note added`);
+    res.json(newNote);
   } else {
     res.error('Error in adding note');
 }});
+
+// notes.get("/", (req, res) => {
+//   readFromFile('./db/db.json')
+
+//   .then((data) => res.json(JSON.parse(data)))
+//   .catch((err) => res.json(err));
+// });
+
+// notes.delete("/:id", (req, res) => {
+//   const id = req.params.id;
+//   readFromFile('./db/db.json')
+//   .then((data) => JSON.parse(data))
+//   .then((data) => data.filter((note) => note.id!== id))
+//   .then((data) => res.json(data))
+//   .catch((err) => res.json(err))
+//   readAndAppend(id, './db/db.json');
+//   res.json(id);
+// });
+
+// notes.put("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const changes = req.body;
+//   readFromFile('./db/db.json')
+//   .then((data) => JSON.parse(data))
+//   .then((data) => data.filter((note) => note.id!== id))
+//   .then((data) => data.push(changes))
+
+//  .then((data) => res.json(data))
+//  .catch((err) => res.json(err));
+
+
+//   readAndAppend(changes, './db/db.json');
+//   res.json(changes);
+// });
 
 module.exports = notes;
