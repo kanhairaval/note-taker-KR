8319e3069c121452401060815ffe37bca79cff49
kanhairaval <kanhairaval@gmail.com>
2023-01-25 02:00:47 -0500

    notes wont save

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,3 +1,18 @@
 const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+notes.post("/", (req, res) => {
+  res.json(req.body);
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+  }
+
+  readAndAppend(newTip, './db/tips.json');
+    res.json(`Node added`);
+  } else {
+    res.error('Error in adding note');
+}});
 
 module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
